7ce5ca794dc9a99e44ddbfce83c7668252f083de
OpticGenius <opticgenius@hotmail.com>
2018-03-24 23:13:10 +1100

    Added Charity names

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import './App.css';
 const VALUES = ["10-10-1997","10-10-1998","10-10-1999", "10-10-2000", "10-10-2001",
                 "10-10-2002", "10-10-2003", "10-10-2004"];
 
+const TEXT = ["Charity 1", "Charity 2", "Charity 3", "Charity 4", "Charity 5", "Charity 6", "Charity 7", "Charity 8"]
+
 const divStyle = {
   width: '100%', 
   height: '100px', 
@@ -23,13 +25,21 @@ class App extends Component {
         <div style={divStyle}>
           <HorizontalTimeline
             index={this.state.value}
+
             indexClick={(index) => {
               this.setState({ value: index, previous: this.state.value });
             }}
+
             values={VALUES} />
         </div>
+
+        <div>
+          {TEXT[this.state.value]}
         </div>
+      </div>
+
     );
+
   }
 }
 
