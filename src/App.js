337e372297ed59587dd0056d77a75d9548405c4b
OpticGenius <opticgenius@hotmail.com>
2018-03-24 16:38:24 +1100

    More changes added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HorizontalTimeline from "./Components/HorizontalTimeline";
 
 import './App.css';
 
-const VALUES = ["10-10-1997","10-10-1998", "10-10-1999", "10-10-2000", "10-10-2001",
+const VALUES = ["10-10-1997","10-10-1998","10-10-1999", "10-10-2000", "10-10-2001",
                 "10-10-2002", "10-10-2003", "10-10-2004"];
 
 class App extends Component {
