d5ed18ade2508df83fc7ca095d457537708b266b
Alex Vosnakis <alex.vosnakis@alexsolutions.com.au>
2018-03-25 10:48:20 +1100

    added social media integration

--- src/Components/CustomButtons/ListButtons.jsx
+++ src/Components/CustomButtons/ListButtons.jsx
@@ -1,21 +1,69 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Menu, { MenuItem } from 'material-ui/Menu';
 import List, {ListItem, ListItemIcon, ListItemText} from 'material-ui/List';
 import AttachMoney from 'material-ui-icons/AttachMoney';
 import Share from 'material-ui-icons/Share';
 import School from 'material-ui-icons/School';
+import { SocialIcon } from 'react-social-icons';
 
-export const ShareButton = props => {
-  return (
-    <ListItem button>
-      <ListItemIcon>
-        <Share />
-      </ListItemIcon>
-    <ListItemText primary="Share" />
-    </ListItem>
-  );
-};
+export class ShareButton extends React.Component {
+
+  state = {
+    anchorEl: null,
+  };
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { anchorEl } = this.state;
+    const menuItemStyle = {
+      height: '40px',
+    }
+
+    return (
+      <div>
+        <ListItem
+          button
+          aria-owns={anchorEl ? 'simple-menu' : null}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+        >
+          <ListItemIcon>
+            <Share />
+          </ListItemIcon>
+          <ListItemText primary="Share" />
+        </ListItem>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose}
+        >
+          <MenuItem style={menuItemStyle} onClick={this.handleClose}>
+            <SocialIcon url="https://facebook.com" />
+            &nbsp;&nbsp;&nbsp;Facebook
+          </MenuItem>
+          <MenuItem style={menuItemStyle} onClick={this.handleClose}>
+            <SocialIcon url="https://twitter.com" />
+            &nbsp;&nbsp;&nbsp;Twitter
+          </MenuItem>
+          <MenuItem style={menuItemStyle} onClick={this.handleClose}>
+            <SocialIcon url="https://instagram.com" />
+            &nbsp;&nbsp;&nbsp;Instagram
+          </MenuItem>
+        </Menu>
+      </div>
+    );
+  }
+}
 
 export const DonateButton = props => {
   return (
