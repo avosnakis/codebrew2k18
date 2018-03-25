d15a7846bc863ee6a8598cbc493ca98a98d30713
Alex Vosnakis <alex.vosnakis@alexsolutions.com.au>
2018-03-25 14:46:27 +1100

    charity page buttons now functional

--- src/Components/CharityPage/ButtonHeader.jsx
+++ src/Components/CharityPage/ButtonHeader.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
 
 import Button from 'material-ui/Button';
 import ArrowBack from 'material-ui-icons/ArrowBack';
@@ -17,10 +18,10 @@ export default function ButtonHeader(props) {
   return (
 	  <div style={orgStyle}>	
       <h1>{org}</h1> 
-			<Button style={{width:"100px"}}>
+			<Button style={{width:"100px"}} component={Link} to={"/"}>
 				<ArrowBack />&nbsp;&nbsp;Back
 			</Button>
-			<Button style={{width:"100px"}}>
+			<Button style={{width:"100px"}} component={Link} to={"/donations/" + org}>
 				<AttachMoney />&nbsp;&nbsp;Donate
 			</Button>
     </div>
